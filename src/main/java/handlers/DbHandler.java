31b24bbdbcff878b150232268ad72f8559a0cca1
SMOKE <flameon1989@yandex.ru>
2021-02-07 13:30:06 +0400

    refactoring

--- src/main/java/handlers/DbHandler.java
+++ src/main/java/handlers/DbHandler.java
@@ -10,10 +10,9 @@ import java.util.*;
 /*** Класс для работы с базой данных*/
 public class DbHandler {
     private static final String CON_STR = "jdbc:sqlite:"
-            + Paths.get(System.getProperty("user.dir"), "src", "main","java","db", "word_statistics.db").toString();
-    private static DbHandler instance = null;
+            + Paths.get(System.getProperty("user.dir"), "src", "main", "java", "db", "word_statistics.db").toString();
     private static Connection connection;
-    private int count = 0;
+    private static DbHandler instance = null;
 
     public static synchronized DbHandler getInstance() {
         try {
@@ -29,12 +28,7 @@ public class DbHandler {
     private DbHandler() throws SQLException {
         DriverManager.registerDriver(new JDBC());
         connection = DriverManager.getConnection(CON_STR);
-        connection.setAutoCommit(false);
-        CreateTables();
-//        Statement st = connection.createStatement();
-//        st.execute("PRAGMA journal_mode = \"WAL\";");
-//        st.execute("PRAGMA encoding = \"UTF-8\";");
-//        connection.commit();
+        configDataBase();
     }
 
     private void configDataBase() throws SQLException {
@@ -64,7 +58,7 @@ public class DbHandler {
     public Map<String, Integer> getAllWords(String tempTableName) {
         LinkedHashMap<String, Integer> wordToCount = new LinkedHashMap<String, Integer>();
         try (Statement st = connection.createStatement()) {
-            ResultSet resultSet = st.executeQuery("SELECT word, count FROM last_statistics ORDER BY count DESC, word ASC");
+            ResultSet resultSet = st.executeQuery("SELECT word, count FROM '" + tempTableName + "' ORDER BY count DESC, word ASC");
             while (resultSet.next()) {
                 wordToCount.put(resultSet.getString("word"), resultSet.getInt("count"));
             }
@@ -91,7 +85,7 @@ public class DbHandler {
 
     public void addProduct(Statement st, String tempTableName, String word) {
         try {
-            count = count + st.executeUpdate("INSERT INTO 'last_statistics' ('word') VALUES('" + word + "') " +
+            st.executeUpdate("INSERT INTO '" + tempTableName + "' ('word') VALUES('" + word + "') " +
                     "on conflict (word) do update set count = count + 1;");
             st.executeUpdate("INSERT INTO 'all_statistics' ('word') VALUES('" + word + "') " +
                     "on conflict (word) do update set count = count + 1;");
---
+++ src/main/java/util/RandomGenerator.java
@@ -0,0 +1,7 @@
+package util;
+
+import net.bytebuddy.utility.RandomString;
+
+public interface RandomGenerator {
+    RandomString randomString = new RandomString(5);
+}
